ea19e71e78c14a2de49b5fe48bc02298b0cb6408
Marwa Zaryab <118633267+marwazaryab@users.noreply.github.com>
2024-10-09 23:43:10 -0400

    Add user verification with JSON data

--- ShadowBot.py
+++ ShadowBot.py
@@ -1,6 +1,14 @@
 import discord
 from discord.ext import commands
+import json
 
 intents = discord.Intents.default()
 intents.message_content = True # can read messages
 bot = commands.Bot(command_prefix ='!', intents = intents) # prefix is !
+
+# parse through json file
+def load_registered_users():
+    with open('registered_users.json','r') as file:
+        return json.load(file)
+    
+registered_users = load_registered_users() # dictionary to map emails to team numbers
