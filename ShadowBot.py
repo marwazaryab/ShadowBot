af32e4756f749e491d0f8ade74077fa0edd0d1b5
Marwa Zaryab <118633267+marwazaryab@users.noreply.github.com>
2024-10-11 10:57:43 -0400

    Verify user and output their team number

--- ShadowBot.py
+++ ShadowBot.py
@@ -12,3 +12,10 @@ def load_registered_users():
         return json.load(file)
     
 registered_users = load_registered_users() # dictionary to map emails to team numbers
+
+# verify user based on email address
+async def verify_user(ctx,username:str):
+    if username not in registered_users:
+        await ctx.send("Email not valid")
+    else:
+        await ctx.send("Welcome to GiSO! You are in team {registered_users[username]}") # notify based on team
