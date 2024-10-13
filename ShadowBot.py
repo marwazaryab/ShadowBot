52194a2c1abbc311a9d42891cf5facee36fac50c
Marwa Zaryab <118633267+marwazaryab@users.noreply.github.com>
2024-10-12 20:35:50 -0400

    Fetch discord token through env file

--- ShadowBot.py
+++ ShadowBot.py
@@ -1,6 +1,13 @@
 import discord
 from discord.ext import commands
 import json
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv() # load environment variables
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True # can read messages
@@ -14,8 +21,11 @@ def load_registered_users():
 registered_users = load_registered_users() # dictionary to map emails to team numbers
 
 # verify user based on email address
+@bot.command()
 async def verify_user(ctx,username:str):
     if username not in registered_users:
         await ctx.send("Email not valid")
     else:
         await ctx.send("Welcome to GiSO! You are in team {registered_users[username]}") # notify based on team
+
+bot.run(DISCORD_TOKEN)
